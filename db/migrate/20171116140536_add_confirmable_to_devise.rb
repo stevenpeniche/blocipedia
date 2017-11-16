210d7938c668e00cf98cf58bbdba4e567a45601e
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-16 10:06:32 -0500

    Fixed Migration Errors

--- db/migrate/20171116140536_add_confirmable_to_devise.rb
+++ db/migrate/20171116140536_add_confirmable_to_devise.rb
@@ -1,5 +1,5 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[5.1]
-  # Note: You can't use change, as User.update_all will fail in the down migration
+	# Note: You can't use change, as User.update_all will fail in the down migration
   def up
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
