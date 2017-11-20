b89361bdf123595232a9a412b70e0516787b5c97
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-20 08:57:45 -0500

    Private Wikis: complete

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -1,6 +1,9 @@
 class SubscriptionController < ApplicationController
   def update
 		current_user.role = 'standard'
+		current_user.wikis.each do |wiki|
+			wiki.update_attribute(:private, false)
+		end
 		current_user.save
 
 		redirect_to edit_user_registration_path
--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -16,6 +16,7 @@ class WikisController < ApplicationController
 		@wiki.title = params[:wiki][:title]
 		@wiki.body = params[:wiki][:body]
 		@wiki.user = current_user
+		@wiki.private = params[:wiki][:private]
 
 		if @wiki.save
 			flash[:notice] = "Wiki was saved."
@@ -34,6 +35,7 @@ class WikisController < ApplicationController
 		@wiki = Wiki.find(params[:id])
 		@wiki.title = params[:wiki][:title]
 		@wiki.body = params[:wiki][:body]
+		@wiki.private = params[:wiki][:private]
 		authorize @wiki
 
 		if @wiki.save
